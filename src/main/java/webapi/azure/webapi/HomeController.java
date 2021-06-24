71dd66d78bb0a15893c685dcd15dddcbdedf25d8
bachoang <bachoahoang@yahoo.com>
2021-06-24 01:46:32 -0500

    Update HomeController.java
    Add more routes to handle both Delegated and Application permission token

--- src/main/java/webapi/azure/webapi/HomeController.java
+++ src/main/java/webapi/azure/webapi/HomeController.java
@@ -8,10 +8,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @GetMapping("/webapiB")
+    // Delegated permission route
+    @GetMapping("/webapiA")
     @ResponseBody
     @PreAuthorize("hasAuthority('SCOPE_hello')")
-    public String file() {
+    public String fileA() {
+        return "Response from WebApiA.";
+    }
+
+    // Application permission route
+    @GetMapping("/webapiB")
+    @ResponseBody
+    @PreAuthorize("hasAuthority('APPROLE_app_hello')")
+    public String fileB() {
         return "Response from WebApiB.";
+    }
+
+    // Either Application or Delegated permission route
+    @GetMapping("/webapiC")
+    @ResponseBody
+    @PreAuthorize("hasAnyAuthority('APPROLE_app_hello','SCOPE_hello')")
+    public String fileC() {
+        return "Response from WebApiC.";
     }
 }
